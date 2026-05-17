15bf2b109835541df42bfef2bfebb08ef974f061
Pedro henrique <pedrohenriquec913@gmail.com>
2026-05-17 19:00:48 -0300

    feat: favoritos interativos

--- product_folder/java_script_folder/js_btn_favorite.js
+++ product_folder/java_script_folder/js_btn_favorite.js
@@ -1,7 +1,25 @@
-const btn = document.querySelector('.favorite-btn');
-
 document.querySelectorAll('.favorite-btn').forEach(btn => {
+
   btn.addEventListener('click', () => {
+
     btn.classList.toggle('active');
+
+    const toast = document.createElement('div');
+
+    if(btn.classList.contains('active')) {
+      toast.textContent = '☕ adicionado aos favoritos';
+    } else {
+      toast.textContent = '❌ removido dos favoritos';
+    }
+
+    toast.classList.add('toast');
+
+    document.body.appendChild(toast);
+
+    setTimeout(() => {
+      toast.remove();
+    }, 2000);
+
   });
+
 });
